7d9be3f78ed5071a8139914b9c5a7b0435019985
Gustavo Borges Cardoso <gustavoborgescardoso@Gustavos-MacBook-Pro.local>
2020-10-26 20:22:29 -0300

    operações CRUD das materias

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -1,18 +1,53 @@
 package com.rasmoo.cliente.escola.gradecurricular.controller;
 
+import com.rasmoo.cliente.escola.gradecurricular.entity.MateriaEntity;
+import com.rasmoo.cliente.escola.gradecurricular.repository.IMateriaRepository;
+import com.rasmoo.cliente.escola.gradecurricular.service.IMateriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/materia")
 public class MateriaController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> helloWorldRest() {
-        return ResponseEntity.status(HttpStatus.OK).body("Olá mundo REST");
+    @Autowired
+    private IMateriaRepository materiaRepository;
+
+    @Autowired
+    private IMateriaService materiaService;
+
+    @GetMapping
+    public ResponseEntity<List<MateriaEntity>> litarMaterias() {
+        return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MateriaEntity> consultaMateria(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findById(id).get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materia) {
+        try{
+            this.materiaRepository.save(materia);
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(false);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> excluirMateria(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.materiaService.excluir(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Boolean> atualizarMateria(@RequestBody MateriaEntity materia) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.materiaService.atualizar(materia));
     }
 
 }
